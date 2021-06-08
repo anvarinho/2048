765c7675e5b73f2fb6ecc30b4f4340fc7cc8e37f
anvarinho <anvarinho@gmail.com>
2021-06-08 17:56:09 +0600

    Initial Commit

---
+++ 2048/_048App.swift
@@ -0,0 +1,17 @@
+//
+//  _048App.swift
+//  2048
+//
+//  Created by Anvar Jumabaev on 08.06.21.
+//
+
+import SwiftUI
+
+@main
+struct _048App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
